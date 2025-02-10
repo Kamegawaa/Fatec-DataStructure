aece12cb5f4c6d1f1f1db18dc7b0c6447e218e54
Lucas Pires Kamegawa <126916359+Kamegawaa@users.noreply.github.com>
2025-02-10 13:48:42 +0000

    DE CRIANÇA NÉ NENEM

---
+++ .devcontainer/E1.java
@@ -0,0 +1,11 @@
+public class E1 {
+    public static void main(String[] args) {
+        Object obj1 = "Tipo String";
+        Object obj2 = 1;
+        Object obj3 = 1.50;
+
+        System.out.println("Tipo de Objeto 1: " + obj1.getClass().getSimpleName());
+        System.out.println("Tipo de Objeto 2: " + obj2.getClass().getSimpleName());
+        System.out.println("Tipo de Objeto 3: " + obj3.getClass().getSimpleName());
+    }
+}
---
+++ .devcontainer/E2.java
@@ -0,0 +1,31 @@
+public class E2 {
+    public static void main(String[] args) {
+        // Soma
+        int soma = 10 + 5;
+        System.out.println("Soma: " + soma);
+
+        // Subtração
+        int subtracao = 10 - 5;
+        System.out.println("Subtração: " + subtracao);
+
+        // Multiplicação
+        int multiplicacao = 10 * 5;
+        System.out.println("Multiplicação: " + multiplicacao);
+
+        // Divisão 
+        double divisao = 10 / 5; 
+        System.out.println("Divisão: " + divisao);
+
+        // Divisão inteira
+        int divisaoInteira = 10 / 3;
+        System.out.println("Divisão inteira: " + divisaoInteira);
+
+        // Módulo 
+        int modulo = 11 % 3;
+        System.out.println("Módulo: " + modulo);
+
+        // Exponenciação
+        double exponenciacao = Math.pow(2, 4);
+        System.out.println("Exponenciação: " + exponenciacao);
+    }
+}
